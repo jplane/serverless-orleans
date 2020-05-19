258bb067a3c6128b990229cbced3d37c0c23356c
Josh Lane <jolan@microsoft.com>
2020-05-19 11:01:15 -0400

    new deploy script for webapp + aci

--- azure_deploy.sh
+++ azure_deploy.sh
@@ -15,7 +15,10 @@ create_storage () {
         --resource-group $RG \
         --sku Standard_LRS
 
-    storage_connection_string=$(az storage account show-connection-string --resource-group $RG --name ${NAME}storage --output tsv)
+    storage_connection_string=$(az storage account show-connection-string \
+                                    --resource-group $RG \
+                                    --name ${NAME}storage \
+                                    --output tsv)
 
     # create storage queue
     az storage queue create \
@@ -26,27 +29,13 @@ create_storage () {
 create_acr () {
     # create container registry
     az acr create \
-        --name ${NAME}registry \
         --resource-group $RG \
+        --name ${NAME}registry \
         --sku Basic \
         --admin-enabled true
 
     # get ACR password
     acr_pwd=$(az acr credential show --name ${NAME}registry --query passwords[0].value --output tsv)
-}
-
-upload_container_image () {
-    # build docker image
-    docker build --tag ${NAME}:latest .
-
-    # login to ACR
-    docker login ${NAME}registry.azurecr.io --username ${NAME}registry --password $acr_pwd
-
-    # tag image for ACR
-    docker tag ${NAME}:latest ${NAME}registry.azurecr.io/app:v1
-
-    # push image to ACR
-    docker push ${NAME}registry.azurecr.io/app:v1
 }
 
 create_database () {
@@ -73,7 +62,11 @@ create_database () {
         --service-objective S0
 
     # get database connection string
-    database_connection_string=$(az sql db show-connection-string --server ${NAME}sqlserver --name ${NAME}database --client ado.net --output tsv)
+    database_connection_string=$(az sql db show-connection-string \
+                                    --server ${NAME}sqlserver \
+                                    --name ${NAME}database \
+                                    --client ado.net \
+                                    --output tsv)
 
     # create blob container
     az storage container create \
@@ -88,7 +81,11 @@ create_database () {
         --connection-string $storage_connection_string
 
     # get storage key
-    storage_key=$(az storage account keys list --resource-group $RG --account-name ${NAME}storage --output tsv --query [0].value)
+    storage_key=$(az storage account keys list \
+                    --resource-group $RG \
+                    --account-name ${NAME}storage \
+                    --output tsv \
+                    --query [0].value)
 
     # restore database backup
     az sql db import \
@@ -102,12 +99,78 @@ create_database () {
         --admin-password $DB_PWD
 }
 
-create_web_app () {
+upload_container_images () {
+    # login to ACR
+    docker login ${NAME}registry.azurecr.io --username ${NAME}registry --password $acr_pwd
+
+    # build frontend docker image
+    docker build -t ${NAME}registry.azurecr.io/frontend:v1 -f frontend.dockerfile .
+
+    # push frontend image to ACR
+    docker push ${NAME}registry.azurecr.io/frontend:v1
+
+    # build backend docker image
+    docker build -t ${NAME}registry.azurecr.io/backend:v1 -f backend.dockerfile .
+
+    # push backend image to ACR
+    docker push ${NAME}registry.azurecr.io/backend:v1
+}
+
+create_log_analytics_workspace () {
+    # create workspace
+    az monitor log-analytics workspace create \
+        --resource-group $RG \
+        --workspace-name ${NAME}loganalyticsworkspace
+
+    # get workspace key
+    la_wksp_key=$(az monitor log-analytics workspace get-shared-keys \
+                    --resource-group ${RG} \
+                    --workspace-name ${NAME}loganalyticsworkspace \
+                    --query primarySharedKey \
+                    --output tsv)
+}
+
+create_vnet () {
+    # create vnet and frontend subnet
+    az network vnet create \
+        --resource-group ${RG} \
+        --name ${NAME}vnet \
+        --address-prefix 10.0.0.0/16 \
+        --subnet-name ${NAME}frontendsubnet \
+        --subnet-prefix 10.0.1.0/24
+
+    # create backend subnet
+    az network vnet subnet create \
+        --resource-group ${RG} \
+        --name ${NAME}backendsubnet \
+        --vnet-name ${NAME}vnet \
+        --address-prefix 10.0.2.0/24
+}
+
+create_container_group () {
+    # create container group attached to vnet
+    az container create \
+        --resource-group $RG \
+        --name ${NAME}containergroup \
+        --image ${NAME}registry.azurecr.io/backend:v1 \
+        --registry-username ${NAME}registry \
+        --registry-password $acr_pwd \
+        --environment-variables \
+                ORLEANS_CONFIG=STORAGE \
+                StorageConnectionString="${storage_connection_string}" \
+        --ports 11111 30000 \
+        --vnet ${NAME}vnet \
+        --subnet ${NAME}backendsubnet \
+        --log-analytics-workspace ${NAME}loganalyticsworkspace \
+        --log-analytics-workspace-key $la_wksp_key
+}
+
+create_app_service () {
     # create app service plan
     az appservice plan create \
         --name ${NAME}appserviceplan \
         --resource-group $RG \
-        --number-of-workers 2 \
+        --number-of-workers 1 \
         --sku S1 \
         --is-linux
 
@@ -116,7 +179,7 @@ create_web_app () {
         --resource-group $RG \
         --plan ${NAME}appserviceplan \
         --name ${NAME}appservice \
-        --deployment-container-image-name ${NAME}registry.azurecr.io/app:v1 \
+        --deployment-container-image-name ${NAME}registry.azurecr.io/frontend:v1 \
         --docker-registry-server-user ${NAME}registry \
         --docker-registry-server-password $acr_pwd
 
@@ -138,30 +201,16 @@ create_web_app () {
     az webapp config appsettings set \
         --resource-group $RG \
         --name ${NAME}appservice \
-        --settings ASPNETCORE_ENVIRONMENT=AZURE-STORAGE AzureWebJobsStorage="${storage_connection_string}"
+        --settings \
+            ORLEANS_CONFIG=STORAGE \
+            AzureWebJobsStorage="${storage_connection_string}"
 
-    # setup vnet
-    az network vnet create \
-        --resource-group $RG \
-        --name ${NAME}vnet \
-        --subnet-name ${NAME}subnet
-  
+    # add to vnet
     az webapp vnet-integration add \
         --resource-group $RG \
         --name ${NAME}appservice \
         --vnet ${NAME}vnet \
-        --subnet ${NAME}subnet
-
-    # cycle the web app
-    az webapp stop \
-        --resource-group $RG \
-        --name ${NAME}appservice
-
-    read -t 10 -p "restarting the web app..."
-
-    az webapp start \
-        --resource-group $RG \
-        --name ${NAME}appservice
+        --subnet ${NAME}frontendsubnet
 }
 
 NAME=${1:-serverlessorleans}
@@ -170,5 +219,8 @@ RG=${NAME}-rg
 create_resource_group
 create_storage
 create_acr
-upload_container_image
-create_web_app
+upload_container_images
+create_log_analytics_workspace
+create_vnet
+create_container_group
+create_app_service
